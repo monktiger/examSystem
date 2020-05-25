e88f83b46f7ff4596159f6260da700ebdc7c13d6
1002861502@qq.com <1002861502@qq.com>
2020-05-25 10:59:11 +0800

    增加了弹窗

--- miniprogram/components/card/card.js
+++ miniprogram/components/card/card.js
@@ -11,34 +11,46 @@ Component({
     cardList: {
       type: Object,
       defult: []
-    }, 
     },
+  },
 
-    /**
-     * 组件的初始数据
-     */
-    data: {
-    },
-    // observers:{
-    //   'TabCur'(TabCur) { 
-    //     let that = this //单个监听
-    //     that.setData({
-    //       TabCur:TabCur
-    //     })
-    //   },
-    // },
-    /**
-     * 组件的方法列表
-     */
-    methods: {
-      clickCard: function (e) {
-        let that = this
-        let data = {
-          TabCur: that.data.TabCur
-        }
-        console.log( that.data.TabCur);
-        
-        this.triggerEvent('clickCard',data)
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
+  // observers:{
+  //   'TabCur'(TabCur) { 
+  //     let that = this //单个监听
+  //     that.setData({
+  //       TabCur:TabCur
+  //     })
+  //   },
+  // },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    // 点击大框，跳转页面
+    clickCard: function (e) {
+      let that = this
+      let data = {
+        TabCur: that.data.TabCur
       }
-    }
-  })
+      console.log(that.data.TabCur);
+
+      this.triggerEvent('clickCard', data)
+    },
+  // 模态窗
+  showModal(e) {
+      this.setData({
+        modalName: e.currentTarget.dataset.target
+      })
+    },
+    hideModal(e) {
+      this.setData({
+        modalName: null
+      })
+    },
+  }
+})
